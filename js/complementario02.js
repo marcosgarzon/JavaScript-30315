0fe9ef7f5628699d77e3f8d944a7a1818cfed906
Marcos Garzon <marcosgarzonmdq@gmail.com>
2022-02-27 19:02:15 -0300

    desafio complementario 2 v.final

--- js/complementario02.js
+++ js/complementario02.js
@@ -1,77 +1,51 @@
-
-
+// Ejecutar: Armar lista
 let arrOpcion = [];
-
 let ListaDeCompras = () => {
-    alert("Vamos a realizar una lista de compras");
-    
+    alert("Vamos a realizar una lista de compras"); 
     let term = "n";
     let agregar = null;
-
     while (term == "n") {
       agregar = prompt("Ingrese un producto");
       if (agregar != null) {
         arrOpcion.push(agregar);
-      } // condicional 1
+      } 
       else {
         alert(
           "No siguió la consigna"
         );
       }
-      let contin = confirm("¿Desea agregar otro producto?"); // botón para agregar otro participante o finalizar
+      let contin = confirm("¿Desea agregar otro producto?"); 
       if (!contin) {
         term = "y";
         alert(
           "La lista de compras es la siguiente: " + arrOpcion.join(",  ")
-        ); // condición de salida cuando presione la opción cancelar
+        ); 
         
-      
       }
-    } //termina ciclo
-    
+    }     
       document.getElementById("lis").innerHTML =  `Lista de compras: <li> ${arrOpcion.join("<li>")}`;
-      
-    
     return arrOpcion;
-  }; //termina funcion
- 
-  
+  }; 
 
-
+// Ejecutar: Ingresar precios
 let impList = () => {
-
-
-
-
   class Precio{
     constructor(producto, valor){
         this.producto = producto
         this.valor = parseFloat(valor)
     }
 }
-
-
-
 let arr2 = [];
-
 for (const arrOpcions of arrOpcion) {
     let valor = prompt(`Costo de: ${arrOpcions}`)
     arr2.push(new Precio(arrOpcions, valor))
 }
-
-
-
 let total = 0
 arr2.forEach( p => {
     total += p.valor
 })
-
 alert(total);
-
-
-
 document.getElementById("tot").innerHTML =  `Precio final: $${total}`;
-
 };
 
 
