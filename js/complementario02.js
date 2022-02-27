f8debe05ff25ec6b3ed782b683ffa0be10568e13
Marcos Garzon <marcosgarzonmdq@gmail.com>
2022-02-27 16:10:25 -0300

    re-commit

--- js/complementario02.js
+++ js/complementario02.js
@@ -1,10 +1,4 @@
-//pasos a seguir:
 
-// boton 1 (funcion suprema): Escribir en el html y guardar en un arreglo una lista de compras
-// boton 2: pedir los elementos del arreglo y agregarles un precio
-// boton 3: dar una alerta con el costo final de la compra e imprimirlo en el html
-
-// boton 1: Escribir en el html y guardar en un arreglo una lista de compras
 
 let arrOpcion = [];
 
@@ -46,7 +40,7 @@ let ListaDeCompras = () => {
 
 let impList = () => {
 
-  // para arriba no escribas nada
+
 
 
   class Precio{
@@ -74,38 +68,13 @@ arr2.forEach( p => {
 
 alert(total);
 
-// hasta aca no pongas mas cosas
+
 
 document.getElementById("tot").innerHTML =  `Precio final: $${total}`;
 
 };
 
-/*
-  class Grade{
-    constructor(name, score){
-        this.name = name
-        this.score = parseInt(score)
-    }
-}
 
-let subjects = ['Matematica', 'Fisica', 'Química', 'Artes']
-let grades = []
-
-for (const subject of subjects) {
-    let score = prompt(`Calificación de ${subject}`)
-    grades.push(new Grade(subject, score))
-}
-
-console.log(grades);
-
-let total = 0
-grades.forEach( g => {
-    total += g.score
-})
-
-console.log('promedio: ', total/grades.length);
-
-*/
 
 
 
