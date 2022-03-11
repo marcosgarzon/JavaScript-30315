b833832150f52332f400ee600502815292b6f33b
Marcos Garzon <marcosgarzonmdq@gmail.com>
2022-03-11 12:11:57 -0300

    código optimizado

--- js/complementario03.js
+++ js/complementario03.js
@@ -1,51 +1,32 @@
 let entrada1 = document.getElementById("entrada1");
 let entrada2 = document.getElementById("entrada2");
 let historial = document.getElementById("historial");
+let total = [];
 
 let suma = () => {
-  let ent1 = [];
-  let ent2 = [];
-  let total = [];
-  ent1.push(entrada1.value);
-  ent2.push(entrada2.value);
-  total = +ent1 + +ent2;
+  total = +entrada1.value + +entrada2.value; 
   let listar = document.createElement("li");
-  listar.innerHTML = `<li> ${ent1} + ${ent2} = ${total} </li>`;
+  listar.innerHTML = `<li> ${entrada1.value} + ${entrada2.value} = ${total} </li>`;
   historial.appendChild(listar);
 };
 
 let resta = () => {
-  let ent1 = [];
-  let ent2 = [];
-  let total = [];
-  ent1.push(entrada1.value);
-  ent2.push(entrada2.value);
-  total = +ent1 - +ent2;
+  total = +entrada1.value - +entrada2.value;
   let listar = document.createElement("li");
-  listar.innerHTML = `<li> ${ent1} - ${ent2} = ${total} </li>`;
+  listar.innerHTML = `<li> ${entrada1.value} - ${entrada2.value} = ${total} </li>`;
   historial.appendChild(listar);
 };
 
 let multiplica = () => {
-  let ent1 = [];
-  let ent2 = [];
-  let total = [];
-  ent1.push(entrada1.value);
-  ent2.push(entrada2.value);
-  total = +ent1 * +ent2;
+  total = +entrada1.value * +entrada2.value;
   let listar = document.createElement("li");
-  listar.innerHTML = `<li> ${ent1} x ${ent2} = ${total} </li>`;
+  listar.innerHTML = `<li> ${entrada1.value} x ${entrada2.value} = ${total} </li>`;
   historial.appendChild(listar);
 };
 
 let divide = () => {
-  let ent1 = [];
-  let ent2 = [];
-  let total = [];
-  ent1.push(entrada1.value);
-  ent2.push(entrada2.value);
-  total = +ent1 / +ent2;
+  total = +entrada1.value / +entrada2.value;
   let listar = document.createElement("li");
-  listar.innerHTML = `<li> ${ent1} / ${ent2} = ${total} </li>`;
+  listar.innerHTML = `<li> ${entrada1.value} / ${entrada2.value} = ${total} </li>`;
   historial.appendChild(listar);
 };
